ac8695894d97abbc1469c13a983e45aff448db8b
Ananthdev24796 <ananth.cr7@gmail.com>
2022-09-07 09:47:11 +0530

    Added UC3

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -19,21 +19,33 @@ public class EmployeeWage {
    *Employee Wage - Assume Wage per Hour is 20 - Assume Full Day Hour is 8
    */
 	
+	/*UC3
+	 * Add Part time
+	 *Employee & Wage
+	 * 
+	 */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int random = (int)(Math.random()*2);
+		int random = (int)(Math.random()*3);
 		System.out.println(random);
 		final int WagesPerHours = 20;
 		final int totalWorkingHrs = 8;
 		int dailyWages = 0;
+		int partTimeWorkingHrs = 4;
 		if(random==1) {
 			System.out.println("Employee is Present");
 			dailyWages = WagesPerHours*totalWorkingHrs;
 			System.out.println("Employee Daily Wages"+dailyWages);
-		}else
-		{
+		}else if(random==2) {
+			System.out.println("Employee is Present");
+			dailyWages = WagesPerHours*partTimeWorkingHrs;
+			System.out.println("PartTimeEmployee Daily Wages"+dailyWages);
+			
+		}else{
 			System.out.println("Employee is Absent");
 		}
+		
 
 	}
 
