e5993d22ad24e7d55dac6f08567cd4a1791801b7
Ananthdev24796 <ananth.cr7@gmail.com>
2022-09-07 09:40:34 +0530

    Added UC2

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -9,16 +9,27 @@ package com.bridgelabz;
  */
 public class EmployeeWage {
 
-	/**
+	/**UC1
 	 * Check Employee is
 	 *Present or Absent
 	 */
+	
+  /*UC2
+   * Calculate Daily
+   *Employee Wage - Assume Wage per Hour is 20 - Assume Full Day Hour is 8
+   */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int random = (int)(Math.random()*2);
 		System.out.println(random);
+		final int WagesPerHours = 20;
+		final int totalWorkingHrs = 8;
+		int dailyWages = 0;
 		if(random==1) {
 			System.out.println("Employee is Present");
+			dailyWages = WagesPerHours*totalWorkingHrs;
+			System.out.println("Employee Daily Wages"+dailyWages);
 		}else
 		{
 			System.out.println("Employee is Absent");
